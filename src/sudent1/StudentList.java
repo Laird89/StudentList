f0662680ec511ff9b8ba5bbb81f829e234ae8588
Laird89 <117320855+Laird89@users.noreply.github.com>
2023-01-24 11:12:52 -0500

    added Date and PULL comment

--- src/sudent1/StudentList.java
+++ src/sudent1/StudentList.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Date: Jan 24th 2023 for PULL command
  */
 package sudent1;
 
